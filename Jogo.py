35950ae0c0c3f8d75dc8ad14545798b892975edf
ViniNovack <vininomn@gmail.com>
2026-04-10 16:47:33 -0300

    Atualização sobre o trabalho feito na opção de jogo de humano X Bot

--- Jogo.py
+++ Jogo.py
@@ -1,5 +1,3 @@
-import os
-
 def humanoHumano():
 
     print("\nVocê selecionou o modo: HUMANO x HUMANO"
@@ -27,6 +25,74 @@ def humanoHumano():
         jogada1 = int(input("Faça sua jogada {jogador1}: "))
 
 
+def humanoBot():
+    tabj = 0
+    tabb = 0
+
+
+    os.system('cls')
+    cnt = 0
+    print('Você selecionou o modo: "JOGAR CONTRA BOT"\n')
+    j1 = str(input('Digite o seu nome: \n'))
+    print(f'Olá {j1}! \n''Eu sou o BOT🤖!, irei ganhar de você!🧐\n')
+    while True:
+        if cnt == 0:
+            cnt +=1
+            v1 = str(input('Vamos COMEÇAR? (Y/N)\n'))
+            if (v1.lower()).strip() == 'n':
+                break
+        else:
+            v2 = str(input('Vamos JOGAR MAIS UMA RODADA? (Y/N)\n'))
+            if (v2.lower()).strip() == 'n':
+                break
+      
+      
+        print('Você pode escolher entre: \n'
+              '1. ✊🏻'
+              '2. 🖐🏻'
+              '3. ✌🏻')
+        rj = int(stdiomask.getpass(prompt='😎 FAÇA SUA JOGADA DE VENCEDOR 🤩: \n', mask='.'))
+        rb = random.randrange(1, 4)
+        if rb == 1:
+            print('🤖👾Eu joguei pedra ✊🏻 \n')
+        elif rb == 2:
+            print('🤖👾Eu joguei papel 🖐🏻 \n')
+        elif rb == 3:
+            print('🤖👾Eu joguei tesoura ✌🏻 \n')
+
+      
+        if rj == rb:
+            print(f'😱NOS EMPATAMOS😩, VAMOS MAIS UMA {j1} QUERO GANHAR DE VOCÊ👾🤩😜')
+            continue
+        elif rj == 1 and rb == 3:
+            print('🤖👾Você ganhou de mim!😡🤬😫,VAMOS MAIS UMA RODADA!!!')
+            tabj +=1
+            continue
+        elif rj == 2 and rb == 1:
+            print('🤖👾Você ganhou de mim!😡🤬😫,VAMOS MAIS UMA RODADA!!!')
+            tabj +=1
+            continue
+        elif rj == 3 and rb == 2:
+            print('🤖👾Você ganhou de mim!😡🤬😫,VAMOS MAIS UMA RODADA!!!')
+            tabj +=1
+            continue
+        elif rb == 1 and rj == 3:
+            print('🤖👾Você é MUITO RUIM, mas não se sinta mal minhas abilidades podem ser muito intimidadoras 🥱😝🤪')
+            tabj +=1
+            continue
+        elif rb == 2 and rj == 1:
+            print('🤖👾Você é MUITO RUIM, mas não se sinta mal minhas abilidades podem ser muito intimidadoras 🥱😝🤪')
+            tabj +=1
+            continue
+        elif rb == 3 and rj == 2:
+            print('🤖👾Você é MUITO RUIM, mas não se sinta mal minhas abilidades podem ser muito intimidadoras 🥱😝🤪')
+            tabj +=1
+            continue
+
+import random
+import os       #(LIMPA O TERMINAL) (os.system('cls'))
+import stdiomask  #(ESCREVE IGUAL SENHA) (stdiomask.getpass(prompt='Escolha uma opção de jogo: \n', mask='.'))
+
 print('OPÇÕES DE JOGO'.center(60))
 print('=-'*30)
 print('1. JOGAR CONTRA BOT'.center(10))
@@ -43,5 +109,3 @@ elif c == 3:
     print('oi')
 else:
     print('TENTE DE NOVO, resposta INVALIDA')
-    os.system('cls')
-    print('limpo')
