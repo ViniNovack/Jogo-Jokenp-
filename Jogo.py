e9909932731beb663f1c1bb750eae516c6cbe0cf
ViniNovack <vininomn@gmail.com>
2026-04-10 10:54:05 -0300

    Criação do menu e criação do esqueleto do codigo

---
+++ Jogo.py
@@ -0,0 +1,20 @@
+import os
+
+print('OPÇÕES DE JOGO'.center(60))
+print('=-'*30)
+print('1. JOGAR CONTRA BOT'.center(10))
+print('2. JOGAR CONTRA AMIGO'.center(10))
+print('3. OBSERVAR DOIS BOTS'.center(10))
+print('=-'*30)
+
+c = int(input('Escolha uma opção de jogo: \n'))
+if c == 1:
+    print('oi')
+elif c == 2:
+    print('oi')
+elif c == 3:
+    print('oi')
+else:
+    print('TENTE DE NOVO, resposta INVALIDA')
+    os.system('cls')
+    print('limpo')
